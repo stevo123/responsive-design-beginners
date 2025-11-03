eefd01c4e4468fb2e185fa4139f2a6b4d425a0ab
Steve Smith <fritzq@mindspring.com>
2025-11-02 21:01:15 -0600

    Add top navigation hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,54 @@
-console.log('Hello world!');
+const main = document.querySelector('#main');
+const footer = document.querySelector('#footer');
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+const menuTopNav = document.querySelector('#menuTopNav');
+const overlay = document.querySelector('#overlay');
+const breakPoint = window.matchMedia('(width < 43.75em)');
+
+const setUpTopNav = () => {
+  if (breakPoint.matches) {
+    // console.log('is mobile');
+    menuTopNav.setAttribute('inert', '');
+  } else {
+    // console.log('is tablet/desktop');
+    menuTopNav.removeAttribute('inert');
+  }
+};
+
+setUpTopNav();
+
+btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
+
+breakPoint.addEventListener('change', () => {
+  // console.log('breakpoint crossed');
+  setUpTopNav();
+});
+
+function openMobileMenu() {
+  // console.log('run openMobileMenu, remove inert');
+  btnOpen.setAttribute('aria-expanded', 'true');
+  main.setAttribute('inert', '');
+  footer.setAttribute('inert', '');
+  menuTopNav.removeAttribute('inert');
+  menuTopNav.style.transitionDuration = '300ms';
+  overlay.style.transitionDuration = '300ms';
+  bodyScrollLock.disableBodyScroll(menuTopNav);
+  btnClose.focus();
+}
+
+function closeMobileMenu() {
+  // console.log('run closeMobileMenu, set inert');
+  btnOpen.setAttribute('aria-expanded', 'false');
+  main.removeAttribute('inert');
+  footer.removeAttribute('inert');
+  menuTopNav.setAttribute('inert', '');
+  bodyScrollLock.enableBodyScroll(menuTopNav);
+  btnOpen.focus();
+
+  setTimeout(() => {
+    menuTopNav.removeAttribute('style');
+    overlay.removeAttribute('style');
+  }, 400);
+}
